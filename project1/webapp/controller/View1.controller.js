545493a1ea16c718113f589eedd13d0435c33083
ravisoni18 <ravisoni18@gmail.com>
2022-06-30 15:14:57 +0530

    Pie chart

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -1,27 +1,44 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
 
-        return Controller.extend("project1.controller.View1", {
-            onInit: function () {
+    return Controller.extend("project1.controller.View1", {
+      onInit: function () {
+        var oModel = new sap.ui.model.json.JSONModel("./model/product.json");
+        //   oModel.loadData("/model/product.json");
+        this.getView().setModel(oModel, "DataModel");
 
-                var oModel = new sap.ui.model.json.JSONModel("./model/product.json");
-            //   oModel.loadData("/model/product.json");
-              this.getView().setModel(oModel, "DataModel");
-
-            //   var oDefaultModel = new sap.ui.model.json.JSONModel();
-            //   oDefaultModel.loadData("/model/SampleData.json");
-              var oModel = new sap.ui.model.json.JSONModel("./model/SampleData.json");
-              this.getView().setModel(oModel);
-            //   this.getView().setModel(oDefaultModel);
-
-
-
+        //   var oDefaultModel = new sap.ui.model.json.JSONModel();
+        //   oDefaultModel.loadData("/model/SampleData.json");
+        var oModel = new sap.ui.model.json.JSONModel("./model/SampleData.json");
+        this.getView().setModel(oModel);
+        //   this.getView().setModel(oDefaultModel);
+        var oVizFrame = this.oVizFrame = this.getView().byId("idVizFrame");
+        oVizFrame.setVizProperties({
+            legend: {
+                title: {
+                   
+                    visible: true
+                }
+            },
+            title: {
+                text:"Sales Revenue by Customers in 2021",
+                visible: true
+            },
+            plotArea: {
+                dataLabel: {
+                    visible: true
+                }
             }
         });
+
+        var dataModel = new sap.ui.model.json.JSONModel("./model/medium.json");
+        oVizFrame.setModel(dataModel);
+      },
     });
+  }
+);
