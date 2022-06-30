e98559a0cf705b2af6c05875428803e95b9fea26
ravisoni18 <ravisoni18@gmail.com>
2022-06-30 14:39:26 +0530

    commit

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -10,13 +10,13 @@ sap.ui.define([
         return Controller.extend("project1.controller.View1", {
             onInit: function () {
 
-                var oModel = new sap.ui.model.json.JSONModel("/model/product.json");
+                var oModel = new sap.ui.model.json.JSONModel("./model/product.json");
             //   oModel.loadData("/model/product.json");
               this.getView().setModel(oModel, "DataModel");
 
             //   var oDefaultModel = new sap.ui.model.json.JSONModel();
             //   oDefaultModel.loadData("/model/SampleData.json");
-              var oModel = new sap.ui.model.json.JSONModel("/model/SampleData.json");
+              var oModel = new sap.ui.model.json.JSONModel("./model/SampleData.json");
               this.getView().setModel(oModel);
             //   this.getView().setModel(oDefaultModel);
 
